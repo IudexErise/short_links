d35f2e4cd96cf7f68a36dd7d92076436e8bc9137
Vyacheslav Saprykin <vyacheslav.saprykin@gmail.com>
2023-03-23 15:59:08 +0600

    links abbr

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Hashids from 'hashids';
 
 function App() {  
 
-  const [id, setId] = useState('');
+  const [uuid, setUuid] = useState('');
   const [url, setUrl] = useState('');
-  const [name, setName] = useState('');
-  const [type, setType] = useState('');
-  const [newUrl, setNewUrl] = useState('');
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('Ссылка');
+  const [shortLink, setShortLink] = useState('');
   
   const SITE_URL = 'https://c2g.su/';
 
   function setNewId() {
     let newId = uuidv4();
-    setId(newId);
-    let convert = new Hashids(newId.substring(0, 8)).encode(1, 2, 3);
-    setNewUrl(SITE_URL + convert)
+    setUuid(newId);
+    let hashedLink = new Hashids(newId).encode(1, 2, 3);
+    setShortLink(SITE_URL + hashedLink)
   }  
 
   let json = {
-    id: id,
-    base_url: url,
-    name: name, 
-    type: type       
+    base_url: url,    
+    category: category,    
+    title: title, 
+    uuid: uuid,
+    short_link: shortLink      
   }  
+
+  useEffect(() => {
+    setNewId();
+  }, []) 
 
   return (
     <div className='container'>
 
       <div>
         <input 
-          value={id} 
-          onChange={e => setId(e.target.value)} 
-          onBlur={() => setNewId()}
+          value={uuid} 
+          onChange={e => setUuid(e.target.value)} 
         />
         <button onClick={() => setNewId()}>Сгенерировать</button>
       </div>
 
-      <select value={type} onChange={(e) => setType(e.target.value)}>
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
         <option value='Вакансия'>Вакансия</option>
         <option value='Мероприятие'>Мероприятие</option>
         <option value='Проект'>Проект</option>
@@ -54,17 +58,17 @@ function App() {
         onChange={e => setUrl(e.target.value)}  
       />
 
-      <label htmlFor="name">Название:</label>
+      <label htmlFor="title">Название:</label>
       <input 
-        id="name"
-        value={name} 
-        onChange={e => setName(e.target.value)}        
+        id="title"
+        value={title} 
+        onChange={e => setTitle(e.target.value)}        
       />
 
-      <span id="link">{newUrl}</span>
-      <button>Сохранить</button>
+      <span>{shortLink}</span>
+      <button onClick={() => alert(JSON.stringify(json))}>Сохранить</button>
 
-      <span id="json">{JSON.stringify(json)}</span>
+      <span>{JSON.stringify(json)}</span>
 
     </div>
   );
